5bbf70ac43697ae1ed6beaf4bbc608adca2bb550
archit910 <architchauhan910@gmail.com>
2016-07-17 22:13:11 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/hptouchsmart/todolist/MainActivity.java
+++ app/src/main/java/com/example/hptouchsmart/todolist/MainActivity.java
@@ -58,6 +58,7 @@ public class MainActivity extends AppCompatActivity {
         adapter = new databaseAdapter(list);
         tasksInDatabase();
         listView.setAdapter(adapter);
+        // this is archit 
 
 
 
